2eb2b833bdcc25e1696be6cb029263651df99b04
plex30 <elplan_be@gmail.com>
2021-11-13 23:32:40 +0100

    develop data slider

--- includes/dbHandler.php
+++ includes/dbHandler.php
@@ -1,5 +1,7 @@
 <?php
 
+use function MongoDB\BSON\toJSON;
+
 require_once dirname(__FILE__) . '/connect.php';
 require __DIR__ . './../vendor/autoload.php';
 
@@ -19,32 +21,100 @@ class dbHandler{
              
         } catch (MongoDB\Exception\Exception $e) {
         
-            /* $filename = basename(__FILE__);
+            $filename = basename(__FILE__);
             
             echo "The $filename script has experienced an error.\n"; 
             echo "It failed with the following exception:\n";
             
             echo "Exception:", $e->getMessage(), "\n";
             echo "In file:", $e->getFile(), "\n";
-            echo "On line:", $e->getLine(), "\n";     */   
+            echo "On line:", $e->getLine(), "\n";     
         }
         
 
     }
 
+   
     public function getAllBancos(){
-       
+        
         $this->collection = $this->connection->dbRastreator->bancos;
       
+      
+        $cursor = $this->collection->find();
+        foreach ($cursor as $document) {
+            $bson = MongoDB\BSON\fromPHP($document);
+            $response[]= json_decode(MongoDB\BSON\toJSON($bson), true);
+        }
 
-        print_r($this->collection->find()->toArray());
+        foreach ($response as $key => $value) {
+            foreach ($value as $key => $val) {
+                if ($key != '_id') { 
+                    if ($val['max'] >= 10000) {
+                       
+                        $json[] =["name"=>$key] + $val;
+                       
+                    }    
+                }
+            }
+        }
+        
+        usort($json, function ($a, $b)
+        {
+            $t1 = $a['max'];
+            $t2 = $b['max'];
+            return $t1 - $t2;
+        }    );
+
+
+        return var_dump($json);
+    
     }
 
     public function getAllSeguros(){
        
         $this->collection = $this->connection->dbRastreator->seguros;
       
+        $cursor = $this->collection->find();
 
-        print_r($this->collection->find()->toArray());
+        foreach ($cursor as $document) {
+            $bson = MongoDB\BSON\fromPHP($document);
+            $response[]= json_decode(MongoDB\BSON\toJSON($bson), true);
+        }
+        foreach ($response as $key => $value) {
+            foreach ($value as $key => $val) {
+                if ($key != '_id') { 
+                    $jsonBasic[] = ["name"=>$key]+$val['basic']; 
+                    $jsonPlus[] = ["name"=>$key]+$val['plus']; 
+                    $jsonFull[] = ["name"=>$key]+$val['full']; 
+                }
+            }
+        }
+        usort($jsonBasic, function ($a, $b)
+        {
+            $t1 = $a['price_from'];
+            $t2 = $b['price_from'];
+            return $t1 - $t2;
+        }    );
+
+        usort($jsonPlus, function ($a, $b)
+        {
+            $t1 = $a['price_from'];
+            $t2 = $b['price_from'];
+            return $t1 - $t2;
+        }    );
+
+        usort($jsonFull, function ($a, $b)
+        {
+            $t1 = $a['price_from'];
+            $t2 = $b['price_from'];
+            return $t1 - $t2;
+        }    );
+
+        $json = [$jsonBasic[0], $jsonPlus[0], $jsonFull[0]];
+
+        return var_dump($json);
+        
     }
+
+  
 }
